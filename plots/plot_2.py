6205a92cef699cb4c425b1df8e6be3e122253f78
evelynsugi <evelynsugihermanto@yahoo.com>
2022-03-02 00:50:58 -0800

    add stylesheet and align center radiobutton

--- plots/plot_2.py
+++ plots/plot_2.py
@@ -1,24 +1,30 @@
 from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
-
 import pandas as pd
+import dash_bootstrap_components as dbc
 
+# Read data
 df = pd.read_csv('../data/Superstore.csv')
 
-app = Dash(__name__)
+# Create instance of app
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+# Layout
 app.layout = html.Div([
     dcc.Graph(id='pie-graph-with-radio', style={'width': 500, 'height': 500}),
-    dcc.RadioItems(
+    dbc.Row(
+        dbc.Col(
+            [dcc.RadioItems(
                 options=['Sales', 'Profit', 'Quantity'],
                 value='Sales',
                 id='metrics',
-                inline=False
-            )
-
+                inline=False,
+                inputStyle={"margin-left": "20px", "margin-right": "2px"})], 
+            width={"size": 6, "offset": 1}
+    ))
 ])
 
-
+# Callback
 @app.callback(
     Output('pie-graph-with-radio', 'figure'),
     Input('metrics', 'value'))
