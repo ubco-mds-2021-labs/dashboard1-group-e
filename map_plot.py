d45db12f973ba3036479a5a4aa0e208e41124291
jufil19 <45440434+jufil19@users.noreply.github.com>
2022-03-02 23:00:51 -0800

    Map now reflects drop down and 0 are gray

--- map_plot.py
+++ map_plot.py
@@ -13,31 +13,37 @@ import dash_bootstrap_components as dbc
 # Reading in data
 df = pd.read_csv("Superstore.csv")
 
-# Removing outlier
-df = df.drop(df[df.Profit == -6599.9780].index)
+# Wrangling data (made it a function, because have to rewrangle data when doing map --> see update_data() function)
+def wrangle_data(unwrangled_data):
+    # Removing outlier
+    df = unwrangled_data.drop(
+        unwrangled_data[unwrangled_data.Profit == -6599.9780].index
+    )
 
-# To sum profit and sales over all cities, for each state
-df = df.groupby(
-    ["State", "Category", "Sub-Category", "Ship Mode", "Segment"], as_index=False
-)[["Sales", "Profit"]].sum()
-df = pd.DataFrame(df)
+    # To sum profit and sales over all cities, for each state
+    df = df.groupby(
+        ["State", "Category", "Sub-Category", "Ship Mode", "Segment"], as_index=False
+    )[["Sales", "Profit"]].sum()
+    df = pd.DataFrame(df)
 
-# Adding column for profit margin
-df["Profit Margin"] = df["Profit"] / df["Sales"]
+    # Adding column for profit margin
+    df["Profit_Margin"] = df["Profit"] / df["Sales"]
 
-# Adding id (ansi code) to corresponding state in order to do chloropleth map
+    # For rows that don't have sales (0/0 = NaN --> replace with 0)
+    df = df.fillna(0)
 
-# https://stackoverflow.com/questions/66892810/using-transform-lookup-for-an-altair-choropleth-figure
-states = alt.topo_feature(data.us_10m.url, feature="states")
+    # Adding id (ansi code) to corresponding state in order to do chloropleth map
+    # per https://gist.github.com/mbostock/4090848#gistcomment-2102151
+    ansi = pd.read_csv("https://www2.census.gov/geo/docs/reference/state.txt", sep="|")
+    ansi.columns = ["id", "abbr", "State", "statens"]
+    ansi = ansi[["id", "abbr", "State"]]
 
-# per https://gist.github.com/mbostock/4090848#gistcomment-2102151
-ansi = pd.read_csv("https://www2.census.gov/geo/docs/reference/state.txt", sep="|")
-ansi.columns = ["id", "abbr", "State", "statens"]
-ansi = ansi[["id", "abbr", "State"]]
+    # getting the id to match with the state from the original dataframe
+    df = pd.merge(df, ansi, how="left", on="State")
+    return df
 
-# getting the id to match with the state from the original dataframe
-df = pd.merge(df, ansi, how="left", on="State")
 
+df = wrangle_data(df)
 
 #########################################
 ######## Layout Elements ################
@@ -46,12 +52,68 @@ df = pd.merge(df, ansi, how="left", on="State")
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 ############ Cards to display info on top of map ##############
+
+############# info in cards #########
+
+
+def info_cards(
+    ship_mode,
+    segment,
+    state,
+    category,
+    sub_category,
+):
+    sales = df.loc[
+        (df["Ship Mode"] == ship_mode)
+        & (df["Segment"] == segment)
+        & (df["State"] == state)
+        & (df["Category"] == category)
+        & (df["Sub-Category"] == sub_category),
+        "Sales",
+    ]
+
+    profit = df.loc[
+        (df["Ship Mode"] == ship_mode)
+        & (df["Segment"] == segment)
+        & (df["State"] == state)
+        & (df["Category"] == category)
+        & (df["Sub-Category"] == sub_category),
+        "Profit",
+    ]
+
+    margin = df.loc[
+        (df["Ship Mode"] == ship_mode)
+        & (df["Segment"] == segment)
+        & (df["State"] == state)
+        & (df["Category"] == category)
+        & (df["Sub-Category"] == sub_category),
+        "Profit_Margin",
+    ]
+
+    if len(sales) != 0:
+        sales_formatted = "${:,.2f}".format(sales[0])
+        profit_formatted = "${:,.2f}".format(profit[0])
+        margin_formatted = "{:,.2f}%".format(margin[0] * 100)
+
+    else:
+        sales_formatted = "-"
+        profit_formatted = "-"
+        margin_formatted = "-"
+
+    return sales_formatted, profit_formatted, margin_formatted
+
+
+##### Container for cards ########
+
 sales_card = dbc.Card(
     [
         dbc.CardHeader("Sales", class_name="text-center"),
         dbc.CardBody(
             [
-                html.H4("Card title", className="text-center", id="sales_card"),
+                html.H4(
+                    className="text-center",
+                    id="sales_card",
+                ),
             ]
         ),
     ]
@@ -62,7 +124,7 @@ profit_card = dbc.Card(
         dbc.CardHeader("Profit", class_name="text-center"),
         dbc.CardBody(
             [
-                html.H4("Card title", className="text-center", id="profit_card"),
+                html.H4(className="text-center", id="profit_card"),
             ]
         ),
     ]
@@ -72,7 +134,7 @@ margin_card = dbc.Card(
         dbc.CardHeader("Profit Margin", class_name="text-center"),
         dbc.CardBody(
             [
-                html.H4("Card title", className="text-center", id="margin_card"),
+                html.H4(className="text-center", id="margin_card"),
             ]
         ),
     ]
@@ -81,39 +143,140 @@ margin_card = dbc.Card(
 ############ Map ###############
 
 
-def plot_map(metric):
+def update_data(
+    ship_mode,
+    segment,
+    category,
+    sub_category,
+):
+    """Appends lines for the states that have no sales for the specified combination of arguments (thus not present in the original dataframe).
+        Their sales and profit are set to 0.
+
+    Args:
+        ship_mode (str, optional): Mode of shipment (can be one of First Class, Second Class, Same Day or Standard Class). Defaults to "First Class".
+        segment (str, optional): . Component of a business (can be one of Consumer, Corporate or Home Office). Defaults to "Consumer".
+        category (str, optional): Category of product (can be one of Furniture, Office Supplies or Technology). Defaults to "Furniture".
+        sub_category (str, optional): Sub-Category of product (ex: sub-category of Furniture is Chairs). Defaults to "Bookcases".
+
+    Returns:
+        pandas dataframe: Returns a dataframe with all the rows resulting from the selected parameters for all the States. The states that
+                          don't have sales for this combination of arguments have sales/profit/profit margin equal to 0.
+    """
+
+    all_states = [state for state in df["State"].unique()]
+
+    # dataframe limited to what the user chooses with the dropdown (excluding state parameter)
+    selected_df = df[
+        (df["Category"] == category)
+        & (df["Sub-Category"] == sub_category)
+        & (df["Ship Mode"] == ship_mode)
+        & (df["Segment"] == segment)
+    ]
+
+    # states that occur at least one for the chosen selection
+    selected_states = [state for state in selected_df["State"].unique()]
+
+    # list of states that are not in our selection (so no sales for this set of parameters)
+    state_no_sales = list(set(all_states).difference(selected_states))
+
+    # Appending lines for the states that have no sales for that specific selection and putting their sales and profit = 0.
+    # If we dont do this, we get a map with blank states. Instead we want the states to have an outline but be considered in the chloropleth as
+    # not having sales
+
+    new_lines = pd.DataFrame(
+        {
+            "State": [state for state in state_no_sales],
+            "Category": [category for i in range(len(state_no_sales))],
+            "Sub-Category": [sub_category for i in range(len(state_no_sales))],
+            "Ship Mode": [ship_mode for i in range(len(state_no_sales))],
+            "Segment": [segment for i in range(len(state_no_sales))],
+            "Sales": [0 for i in range(len(state_no_sales))],
+            "Profit": [0 for i in range(len(state_no_sales))],
+        }
+    )
+
+    updated_data = pd.concat([df, new_lines], ignore_index=True)
+
+    # using wrangle_data() to add the `id` and `abbr` column necessary for the map plot (or else the rows for the appended lines are NaN for these columns)
+    return wrangle_data(updated_data)
+
+
+def plot_map(
+    metric="Sales",
+    ship_mode="First Class",
+    segment="Consumer",
+    category="Furniture",
+    sub_category="Bookcases",
+):
     """_summary_
 
     Args:
         metric (str): Variable according to which we wish to show the chloropleth map (one of either Sales, Profit or Profit Margin)
+        ship_mode (str, optional): Mode of shipment (can be one of First Class, Second Class, Same Day or Standard Class). Defaults to "First Class".
+        segment (str, optional): . Component of a business (can be one of Consumer, Corporate or Home Office). Defaults to "Consumer".
+        category (str, optional): Category of product (can be one of Furniture, Office Supplies or Technology). Defaults to "Furniture".
+        sub_category (str, optional): Sub-Category of product (ex: sub-category of Furniture is Chairs). Defaults to "Bookcases".
 
     Returns:
         altair chart: A map of the United States where each state is colored in proportion to the metric chosen.
     """
 
+    # https://stackoverflow.com/questions/66892810/using-transform-lookup-for-an-altair-choropleth-figure
+    states = alt.topo_feature(data.us_10m.url, feature="states")
+
     highlight = alt.selection_single(on="click", fields=["State"], empty="none")
 
-    if metric == "Profit Margin":
+    # Formatting for tooltip and chloropleth legend (depends on whether we have sales/proft ($) or margin (%))
+    if metric == "Profit_Margin":
         metric_format = ".0%"
+        # metric_color = "yelloworangered"
     else:
         metric_format = "$.0f"
+        # metric_color = "redblue"
+
+    updated_df = update_data(
+        ship_mode=ship_mode,
+        segment=segment,
+        category=category,
+        sub_category=sub_category,
+    )
+
     chart = (
         alt.Chart(states)
         .mark_geoshape(stroke="black")
         .encode(
-            stroke=alt.condition(highlight, alt.value("black"), alt.value("#ffffff00")),
-            color=alt.Color(
+            stroke=alt.value(
+                "black"
+            ),  # alt.condition(highlight, alt.value("black"), alt.value("#ffffff00")),
+            color=alt.condition(
+                f"datum.{metric} != 0",
                 f"{metric}:Q",
+                alt.value("lightgray"),
                 legend=alt.Legend(format=metric_format),
-                scale=alt.Scale(scheme="purpleblue"),
+                scale=alt.Scale(scheme="redblue"),
             ),
+            # color=alt.Color(
+            #     f"{metric}:Q",
+            #     legend=alt.Legend(format=metric_format),
+            #     scale=alt.Scale(scheme="redblue"),
+            # ),
             tooltip=[
                 alt.Tooltip("State:N"),
                 alt.Tooltip(f"{metric}:Q", format=metric_format),
             ],
         )
         .transform_lookup(
-            lookup="id", from_=alt.LookupData(df, "id", [metric, "State"])
+            lookup="id",
+            from_=alt.LookupData(
+                updated_df[
+                    (updated_df["Category"] == category)
+                    & (updated_df["Sub-Category"] == sub_category)
+                    & (updated_df["Ship Mode"] == ship_mode)
+                    & (updated_df["Segment"] == segment)
+                ],
+                "id",
+                [metric, "State"],
+            ),
         )
         .properties(width=500, height=300)
         .add_selection(highlight)
@@ -142,7 +305,7 @@ app.layout = dbc.Container(
             [
                 dbc.Col(
                     html.Iframe(
-                        srcDoc=plot_map(metric="Sales"),
+                        srcDoc=plot_map(),
                         id="map",
                         style={"width": "100%", "height": "400px"},
                     ),
@@ -157,7 +320,7 @@ app.layout = dbc.Container(
                     id="radiobutton_map",
                     options=[
                         {"label": "Profit", "value": "Profit"},
-                        {"label": "Profit Margin", "value": "Profit Margin"},
+                        {"label": "Profit Margin", "value": "Profit_Margin"},
                         {"label": "Sales", "value": "Sales"},
                     ],
                     value="Sales",
@@ -173,7 +336,7 @@ app.layout = dbc.Container(
                     dcc.Dropdown(
                         placeholder="Select a state",
                         id="dropdown_state",
-                        # value="Colorado",  # REQUIRED to show the plot on the first page load
+                        value="Louisiana",  # REQUIRED to show the plot on the first page load
                         options=[
                             {"label": state, "value": state}
                             for state in sorted(df["State"].unique())
@@ -184,7 +347,7 @@ app.layout = dbc.Container(
                     dcc.Dropdown(
                         placeholder="Select a shipment mode",
                         id="dropdown_ship_mode",
-                        # value="First Class",  # REQUIRED to show the plot on the first page load
+                        value="First Class",  # REQUIRED to show the plot on the first page load
                         options=[
                             {"label": mode, "value": mode}
                             for mode in sorted(df["Ship Mode"].unique())
@@ -195,7 +358,7 @@ app.layout = dbc.Container(
                     dcc.Dropdown(
                         placeholder="Select a segment",
                         id="dropdown_segment",
-                        # value="Consumer",  # REQUIRED to show the plot on the first page load
+                        value="Consumer",  # REQUIRED to show the plot on the first page load
                         options=[
                             {"label": seg, "value": seg}
                             for seg in sorted(df["Segment"].unique())
@@ -206,7 +369,7 @@ app.layout = dbc.Container(
                     dcc.Dropdown(
                         placeholder="Select a category",
                         id="dropdown_category",
-                        # value="Furniture",  # REQUIRED to show the plot on the first page load
+                        value="Furniture",  # REQUIRED to show the plot on the first page load
                         options=[
                             {"label": cat, "value": cat}
                             for cat in sorted(df["Category"].unique())
@@ -217,12 +380,8 @@ app.layout = dbc.Container(
                     dcc.Dropdown(
                         placeholder="Select a sub-category",
                         id="dropdown_sub_category",
-                        options=[]
-                        # value="Bookcases",  # REQUIRED to show the plot on the first page load
-                        # options=[
-                        #     {"label": sub, "value": sub}
-                        #     for sub in sorted(df["Sub-Category"].unique())
-                        # ],
+                        options=[],
+                        value="Bookcases",  # REQUIRED to show the plot on the first page load
                     )
                 ),
             ]
@@ -299,7 +458,7 @@ def update_cards(ship_mode, segment, state, category, sub_category):
         & (df["State"] == state)
         & (df["Category"] == category)
         & (df["Sub-Category"] == sub_category),
-        "Profit Margin",
+        "Profit_Margin",
     ]
 
     if len(sales) != 0:
@@ -316,9 +475,20 @@ def update_cards(ship_mode, segment, state, category, sub_category):
 
 
 # Updates map
-@app.callback(Output("map", "srcDoc"), Input("radiobutton_map", "value"))
-def update_map(metric):
-    return plot_map(metric)
+
+
+@app.callback(
+    Output("map", "srcDoc"),
+    [
+        Input("radiobutton_map", "value"),
+        Input("dropdown_ship_mode", "value"),
+        Input("dropdown_segment", "value"),
+        Input("dropdown_category", "value"),
+        Input("dropdown_sub_category", "value"),
+    ],
+)
+def update_map(metric, ship_mode, segment, category, sub_category):
+    return plot_map(metric, ship_mode, segment, category, sub_category)
 
 
 if __name__ == "__main__":
